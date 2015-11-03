c7b3e988ba78611aacda8f1af3ae2da71307e979
kwou <kdw.5@hotmail.com>
2015-11-02 19:19:13 -0800

    Removed need to match old password

--- Q4/Password Changer/54724117-password-changer.py
+++ Q4/Password Changer/54724117-password-changer.py
@@ -5,33 +5,22 @@ def main():
     pw_offset = 75779 # 0x12803 - offset of hashed password in file
     filename = "54724117.program2.exe"
     
-    old_pw = input('Enter old password: ')
-    h = get_sha1_hash(old_pw)
+    new_pw = input('Enter new password: ')
+    new_pw_confirm = input('Confirm password: ')
 
-    # Confirm the entered password matches the old password
-    f = open(filename, 'rb+')
-    f.seek(pw_offset)
-    stored_pw = f.read(20);
-    f.close()
-
-    # Only allow changing of the password if old password matched
-    if h != stored_pw:
-        print("Password does not match!")
+    # Ensure the two entered passwords matched before changing
+    if new_pw != new_pw_confirm:
+        print("Passwords do not match!")
+    elif len(new_pw) == 0:
+        print("Password must be longer than 0 characters!")
     else:
-        new_pw = input('Enter new password: ')
-        new_pw_confirm = input('Confirm password: ')
-
-        # Ensure the two entered passwords matched before changing
-        if new_pw != new_pw_confirm:
-            print("Passwords do not match!")
-        else:
-            # Set the new password
-            new_hash = get_sha1_hash(new_pw)
-            f = open(filename, 'rb+')
-            f.seek(pw_offset)
-            f.write(new_hash)
-            f.close()
-            print ("Password Changed!")
+        # Set the new password
+        new_hash = get_sha1_hash(new_pw)
+        f = open(filename, 'rb+')
+        f.seek(pw_offset)
+        f.write(new_hash)
+        f.close()
+        print ("Password Changed!")
 
 def get_sha1_hash(str):
     sh = hashlib.sha1()
