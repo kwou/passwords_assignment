4cce69701d851e7595be47f3b4a5a01dd10291d6
kwou <kdw.5@hotmail.com>
2015-11-02 19:02:45 -0800

    Added comments to password changer

--- Q4/Password Changer/54724117-password-changer.py
+++ Q4/Password Changer/54724117-password-changer.py
@@ -2,26 +2,30 @@ import hashlib
 import sys
 
 def main():
-    pw_offset = 75779 # 0x12803
+    pw_offset = 75779 # 0x12803 - offset of hashed password in file
     filename = "54724117.program2.exe"
     
     old_pw = input('Enter old password: ')
     h = get_hash(old_pw)
 
+    # Confirm the entered password matches the old password
     f = open(filename, 'rb+')
     f.seek(pw_offset)
     stored_pw = f.read(20);
     f.close()
 
+    # Only allow changing of the password if old password matched
     if h != stored_pw:
         print("Password does not match!")
     else:
         new_pw = input('Enter new password: ')
         new_pw_confirm = input('Confirm password: ')
 
+        # Ensure the two entered passwords matched before changing
         if new_pw != new_pw_confirm:
             print("Passwords do not match!")
-        else:            
+        else:
+            # Set the new password
             new_hash = get_hash(new_pw)
             f = open(filename, 'rb+')
             f.seek(pw_offset)
